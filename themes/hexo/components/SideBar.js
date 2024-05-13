3ee896c1159aae568ddfbfe8b79660b470bbb036
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-05-13 22:46:58 +1000

    Update SideBar.js

--- themes/hexo/components/SideBar.js
+++ themes/hexo/components/SideBar.js
@@ -16,25 +16,6 @@ const SideBar = props => {
   const router = useRouter()
   return (
     <div id='side-bar'>
-      <div className='h-52 w-full flex justify-center'>
-        <div>
-          <div
-            onClick={() => {
-              router.push('/')
-            }}
-            className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100  transform duration-200 cursor-pointer'>
-            {/* 头像 */}
-            <LazyImage
-              src={siteInfo?.icon}
-              className='rounded-full'
-              width={80}
-              alt={siteConfig('AUTHOR')}
-            />
-          </div>
-          {/* 总览 */}
-          <MenuGroupCard {...props} />
-        </div>
-      </div>
       {/* 侧拉抽屉的菜单 */}
       <MenuListSide {...props} />
     </div>
