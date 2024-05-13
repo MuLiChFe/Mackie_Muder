d76dc0f586de504daa0f7746f53e4dff61b9e8a1
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-05-13 21:34:14 +1000

    Update index.js

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -112,7 +112,7 @@ const LayoutBase = props => {
             {/* 右侧栏 */}
             <SideRight
                 {...props}
-                className={`w-full max-w-6xl undefined mt-6 px-3 mx-auto lg:flex lg:space-x-4 justify-center relative z-10 ${post ? 'lg:pt-0' : 'lg:pt-4'}`}
+                className={`w-full max-w-6xl undefined mt-6 px-3 mx-auto lg:flex lg:justify-center relative z-10 ${post ? 'lg:pt-0' : 'lg:pt-4'}`}
             />
             <div
             id='container-inner'
