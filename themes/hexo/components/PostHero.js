36bb380a673b4ef391e3d527c632470a0e4be071
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-05-14 10:35:02 +1000

    Update PostHero.js

--- themes/hexo/components/PostHero.js
+++ themes/hexo/components/PostHero.js
@@ -57,41 +57,6 @@ export default function PostHero({ post, siteInfo }) {
             )}
             {post.title}
           </div>
-
-          <section className='flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8'>
-            <div className='flex justify-center dark:text-gray-200 text-opacity-70'>
-              {post?.type !== 'Page' && (
-                <>
-                  <Link
-                    href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
-                    passHref
-                    className='pl-1 mr-2 cursor-pointer hover:underline'>
-                    {locale.COMMON.POST_TIME}: {post?.publishDay}
-                  </Link>
-                </>
-              )}
-              <div className='pl-1 mr-2'>
-                {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
-              </div>
-            </div>
-
-            {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && (
-              <div className='busuanzi_container_page_pv font-light mr-2'>
-                <span className='mr-2 busuanzi_value_page_pv' />
-                {locale.COMMON.VIEWS}
-              </div>
-            )}
-          </section>
-
-          <div className='mt-4 mb-1'>
-            {post.tagItems && (
-              <div className='flex justify-center flex-nowrap overflow-x-auto'>
-                {post.tagItems.map(tag => (
-                  <TagItemMini key={tag.name} tag={tag} />
-                ))}
-              </div>
-            )}
-          </div>
         </div>
       </header>
     </div>
