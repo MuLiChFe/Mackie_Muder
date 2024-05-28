9210fff9898e9dbf0c98db8382a46f201116c447
TGCS <76549977+MuLiChFe@users.noreply.github.com>
2024-05-28 19:10:03 +1000

    Update MenuListTop.js

--- themes/hexo/components/MenuListTop.js
+++ themes/hexo/components/MenuListTop.js
@@ -8,29 +8,6 @@ export const MenuListTop = props => {
   const { locale } = useGlobal()
 
   let links = [
-    {
-      id: 1,
-      icon: 'fa-solid fa-house',
-      name: locale.NAV.INDEX,
-      href: '/',
-      show: siteConfig('HEXO_MENU_INDEX', null, CONFIG)
-    },
-    {
-      id: 2,
-      icon: 'fas fa-search',
-      name: locale.NAV.SEARCH,
-      href: '/search',
-      show: siteConfig('HEXO_MENU_SEARCH', null, CONFIG)
-    },
-    {
-      id: 3,
-      icon: 'fas fa-archive',
-      name: locale.NAV.ARCHIVE,
-      href: '/archive',
-      show: siteConfig('HEXO_MENU_ARCHIVE', null, CONFIG)
-    }
-    // { icon: 'fas fa-folder', name: locale.COMMON.CATEGORY, href: '/category', show: siteConfig('MENU_CATEGORY', null, CONFIG) },
-    // { icon: 'fas fa-tag', name: locale.COMMON.TAGS, href: '/tag', show: siteConfig('MENU_TAG', null, CONFIG) }
   ]
 
   // 如果 开启自定义菜单，则覆盖Page生成的菜单
